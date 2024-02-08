6dea860037ac1ce5afb3666bf63b4ff466191655
UkrOS <121632608+UkrOS2022@users.noreply.github.com>
2024-02-08 19:39:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtMultimedia import *
 from PyQt5.QtMultimediaWidgets import *
-from PyQt5.QtWebEngineWidgets import *
 import sys
 import os
 import time
